c9d630e321c594b1e7193b19f27e9a9283006145
milagrosterrel <milaterrel@gmail.com>
2015-11-25 13:11:53 +1200

    primer comit

---
+++ contact.php
@@ -0,0 +1,90 @@
+<?php
+
+if(isset($_POST['email'])) {
+
+    $email_to = "alexandravaldiviario@gmail.com";
+ 
+    $email_subject = "Contacto Web - alexandravaldivia.com";
+
+ 
+    function died($error) {
+ 
+        // your error code can go here
+ 
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+ 
+        echo "These errors appear below.<br /><br />";
+ 
+        echo $error."<br /><br />";
+ 
+        echo "Please go back and fix these errors.<br /><br />";
+ 
+        die();
+ 
+    }
+ 
+     
+ 
+    // validation expected data exists
+ 
+    if(!isset($_POST['name']) ||
+ 
+        !isset($_POST['email']) ||
+ 
+        !isset($_POST['message'])) {
+ 
+        died('We are sorry, but there appears to be a problem with the form you submitted.');       
+ 
+    }
+ 
+     
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+ 
+ 
+    $email_message = "Form details below.\n\n";
+ 
+     
+ 
+    function clean_string($string) {
+ 
+      $bad = array("content-type","bcc:","to:","cc:","href");
+ 
+      return str_replace($bad,"",$string);
+ 
+    }
+ 
+     
+ 
+    $email_message .= "Name: ".clean_string($name)."\n";
+ 
+    $email_message .= "Email: ".clean_string($email)."\n";
+ 
+    $email_message .= "Message: ".clean_string($message)."\n";
+
+ 
+    // create email headers
+     
+    $headers = 'From: '.$email."\r\n".
+     
+    'Reply-To: '.$email."\r\n" .
+     
+    'X-Mailer: PHP/' . phpversion();
+     
+    @mail($email_to, $email_subject, $email_message, $headers);  
+
+
+?>
+ 
+ 
+ 
+<!-- include your own success html here -->
+ 
+ Gracias por su contacto. Me pondre en contacto con Ud. muy pronto o puede escribirme directamente a: alexandravaldiviario@gmail.com 
+ 
+<?php
+ 
+}
+ 
+?>
---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Gelatin over button effect with Sass
+
+// Coding by François Lesenne http://lesenne.fr
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
