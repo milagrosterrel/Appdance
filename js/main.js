36b20c035372a1c7f42ed529215e6b8405e3790d
milagrosterrel <milaterrel@gmail.com>
2015-11-26 17:52:57 +1200

    may

--- js/main.js
+++ js/main.js
@@ -10,29 +10,3 @@ $("#holder").hide();
 $("#holder").fadeIn(2000);
 <<<<<<< HEAD
 
-
-
-=======
-//asociados//
-// based on JQ I found here: http://stackoverflow.com/posts/3842442/revisions
-//initial rotation
-$('.photo').each(function() {
-  var randrot = Math.random() * 20 - 10; //has to be local to run w/ each function call
-  $(this).css('transform', 'rotate(' + randrot + 'deg) scale(1)');
-});
-
-//hover/unhover rotations
-$('.photo').hover(function() {
-  var randrot = Math.random() * 20 - 10; //has to be local to run w/ each function call
-  $(this).css({
-    transform: "rotate(" + randrot + "deg) scale(1.25)",
-    'z-index': "1" //kind of hacky, but standard notation didn't like the '-' in z-index, open to suggestions
-  });
-}, function() {
-  var randrot = Math.random() * 20 - 10; //has to be local to run w/ each function call
-  $(this).css({
-    transform: "rotate(" + randrot + "deg) scale(1)",
-    'z-index': "0" //kind of hacky, but standard notation didn't like the '-' in z-index, open to suggestions
-  });
-});
->>>>>>> 86ae997e568cdc25d1cfb564a19f5bd49abad106
